a044dca159503e15b1df848b35ff2e5d3708b54d
JHLee <leejunhee1219@snu.ac.kr>
2023-09-10 19:24:15 +0900

    asdf

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let vh = window.innerHeight * 0.01; document.documentElement.style.setProperty('--vh', vh+"px");
-window.addEventListener("resize", ()=>{ let vh = window.innerHeight * 0.01; document.documentElement.style.setProperty('--vh', vh+"px"); })
+window.addEventListener("resize", ()=>{ let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', vh+"px");
+   })
+$(document).ready(function(){
+document.documentElement.style.setProperty('--vh', vh+"px");
+});
 
 // Service Worker 등록
 if ('serviceWorker' in navigator) {
